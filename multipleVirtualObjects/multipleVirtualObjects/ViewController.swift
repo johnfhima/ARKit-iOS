d4d3867485243d012ced2f938933da569e8dd7e3
johnfhima <61589415+johnfhima@users.noreply.github.com>
2022-09-03 11:51:03 +0300

    Add files via upload

---
+++ multipleVirtualObjects/multipleVirtualObjects/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  multipleVirtualObjects
+//
+//  Created by Jonathan Fhima on 03/09/2022.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene()
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        let box = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "wood.jpeg")
+        let boxNode = SCNNode(geometry: box)
+        boxNode.geometry?.materials = [material]
+        boxNode.position = SCNVector3(0, 0, -1)
+        
+        
+        self.sceneView.scene.rootNode.addChildNode(boxNode)
+        
+        let sphere = SCNSphere(radius: 0.1)
+        sphere.firstMaterial?.diffuse.contents = UIImage(named: "earth.jpg")
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = SCNVector3(0.3, 0.3, -0.5)
+        self.sceneView.scene.rootNode.addChildNode(sphereNode)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+}
